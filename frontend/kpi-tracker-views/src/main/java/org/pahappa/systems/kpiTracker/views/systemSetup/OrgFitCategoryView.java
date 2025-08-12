8354317d546685dd7391e14374c855eecea0880a
ndjek1 <vndjekornom@gmail.com>
2025-08-12 23:45:57 +0300

    Added beans for org fit category table and dialog form, user interface for the table and the dialog and updated the side bar

---
+++ frontend/kpi-tracker-views/src/main/java/org/pahappa/systems/kpiTracker/views/systemSetup/OrgFitCategoryView.java
@@ -0,0 +1,75 @@
+package org.pahappa.systems.kpiTracker.views.systemSetup;
+
+import com.googlecode.genericdao.search.Search;
+import lombok.Getter;
+import lombok.Setter;
+
+import org.pahappa.systems.kpiTracker.core.services.OrgFitCategoryService;
+import org.pahappa.systems.kpiTracker.models.systemSetup.OrgFitCategory;
+
+import org.pahappa.systems.kpiTracker.security.UiUtils;
+import org.sers.webutils.client.views.presenters.PaginatedTableView;
+import org.sers.webutils.model.RecordStatus;
+import org.sers.webutils.model.exception.OperationFailedException;
+import org.sers.webutils.server.core.service.excel.reports.ExcelReport;
+import org.sers.webutils.server.core.utils.ApplicationContextProvider;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.List;
+import java.util.Map;
+
+@ManagedBean(name = "orgFitCategoryView")
+@Setter
+@Getter
+@SessionScoped
+public class OrgFitCategoryView extends PaginatedTableView<OrgFitCategory, OrgFitCategoryService,OrgFitCategoryService> {
+    private OrgFitCategoryService orgFitService;
+    private Search search;
+
+
+    @PostConstruct
+    public void init(){
+        orgFitService = ApplicationContextProvider.getBean(OrgFitCategoryService.class);
+        reloadFilterReset();
+    }
+    @Override
+    public void reloadFromDB(int i, int i1, Map<String, Object> map) throws Exception {
+        super.setDataModels(orgFitService.getInstances(new Search().addFilterEqual("recordStatus", RecordStatus.ACTIVE),i,i1));
+    }
+
+    @Override
+    public List<ExcelReport> getExcelReportModels() {
+        return null;
+    }
+
+    @Override
+    public String getFileName() {
+        return null;
+    }
+
+    @Override
+    public List load(int i, int i1, Map map, Map map1) {
+        return null;
+    }
+
+    @Override
+    public void reloadFilterReset(){
+        super.setTotalRecords(orgFitService.countInstances(new Search()));
+        try{
+            super.reloadFilterReset();
+        }catch(Exception e){
+            UiUtils.ComposeFailure("Error",e.getLocalizedMessage());
+        }
+
+    }
+
+    public void deleteClient(OrgFitCategory orgFitCategory) {
+        try {
+            orgFitService.deleteInstance(orgFitCategory);
+        } catch (OperationFailedException e) {
+            UiUtils.ComposeFailure("Delete Failed", e.getLocalizedMessage());
+        }
+    }
+}
