4c6ea2e8d387555f6555dfaa6d25e2a470de345d
ndjek1 <vndjekornom@gmail.com>
2025-08-14 11:27:57 +0300

    Added a new feature for managing org fit category items, including the model, service and implementation, and Added organization fit Category converter that helps when attaching an item to a category

---
+++ frontend/kpi-tracker-web/src/main/java/org/pahappa/systems/client/converters/OrgFitCategoryConverter.java
@@ -0,0 +1,31 @@
+package org.pahappa.systems.client.converters;
+
+import org.pahappa.systems.kpiTracker.core.services.OrgFitCategoryService;
+import org.pahappa.systems.kpiTracker.models.systemSetup.OrgFitCategory;
+import org.sers.webutils.model.security.Role;
+import org.sers.webutils.server.core.service.RoleService;
+import org.sers.webutils.server.core.utils.ApplicationContextProvider;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter("orgFitCategoryConverter")
+public class OrgFitCategoryConverter implements Converter {
+    @Override
+    public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) {
+        if (arg2 == null || arg2.isEmpty())
+            return null;
+        return ApplicationContextProvider.getBean(OrgFitCategoryService.class)
+                .getObjectById(arg2);
+    }
+
+    @Override
+    public String getAsString(FacesContext arg0, UIComponent arg1, Object object) {
+        if (object == null || object instanceof String)
+            return null;
+
+        return ((OrgFitCategory) object).getId();
+    }
+}
