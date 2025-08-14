5147c905f9da1d33b24b033a83a9bcfd1ba3b65f
ndjek1 <vndjekornom@gmail.com>
2025-08-14 15:20:21 +0300

    Added backend part of the threshold management feature

---
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/services/impl/ThresholdServiceImpl.java
@@ -0,0 +1,25 @@
+package org.pahappa.systems.kpiTracker.core.services.impl;
+
+import org.pahappa.systems.kpiTracker.core.services.ThresholdService;
+import org.pahappa.systems.kpiTracker.models.systemSetup.Threshold;
+import org.pahappa.systems.kpiTracker.utils.Validate;
+import org.sers.webutils.model.exception.OperationFailedException;
+import org.sers.webutils.model.exception.ValidationFailedException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ThresholdServiceImpl extends GenericServiceImpl<Threshold> implements ThresholdService {
+
+    @Override
+    public Threshold saveInstance(Threshold entityInstance) throws ValidationFailedException, OperationFailedException {
+        Validate.notNull(entityInstance, "Missing details");
+        return save(entityInstance);
+    }
+
+    @Override
+    public boolean isDeletable(Threshold instance) throws OperationFailedException {
+        return true;
+    }
+}
