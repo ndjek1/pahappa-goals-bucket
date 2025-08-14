b7ccada0655ba0f335b8ab36dcf56bae25a7d9ac
ndjek1 <vndjekornom@gmail.com>
2025-08-14 11:10:08 +0300

    Updated the organization fit services and added a migration class that allow to set default values

---
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/services/impl/OrgFitCategoryMigration.java
@@ -0,0 +1,43 @@
+package org.pahappa.systems.kpiTracker.core.services.impl;
+
+
+import org.pahappa.systems.kpiTracker.core.services.OrgFitCategoryService;
+import org.pahappa.systems.kpiTracker.models.systemSetup.OrgFitCategory;
+import org.pahappa.systems.kpiTracker.models.systemSetup.enums.OrgFitCategoryType;
+import org.sers.webutils.model.RecordStatus;
+import org.sers.webutils.model.migrations.Migration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.Date;
+
+@Service
+@Transactional
+public class OrgFitCategoryMigration {
+
+    @Autowired
+    private OrgFitCategoryService orgFitCategory;
+
+    @Migration(orderNumber = 2)
+    public void initializeDefaultOrgFitCategories() {
+        createIfNotExists(OrgFitCategoryType.TEAM_RATING, "Team Rating", "Collaboration, Communication, etc.", 5.0);
+        createIfNotExists(OrgFitCategoryType.COMPANY_VALUES, "Company Values", "Self-management, Resource management, etc.", 20.0);
+        createIfNotExists(OrgFitCategoryType.KEEPER_TEST, "Keeper Test", "Competence, Growth, Innovation", 15.0);
+    }
+
+    private void createIfNotExists(OrgFitCategoryType type, String name, String description, double weight) {
+        if (orgFitCategory.getAllInstances().isEmpty()) {
+            OrgFitCategory category = new OrgFitCategory();
+            category.setType(type);
+            category.setName(name);
+            category.setDescription(description);
+            category.setWeight(weight);
+            category.setRecordStatus(RecordStatus.ACTIVE);
+            category.setDateCreated(new Date());
+            category.setDateChanged(new Date());
+            orgFitCategory.mergeBG(category);
+        }
+    }
+}
