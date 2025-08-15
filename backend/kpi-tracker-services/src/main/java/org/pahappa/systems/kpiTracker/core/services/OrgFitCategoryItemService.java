6562af95f08aef551f815257cebf36f36dc27a78
ndjek1 <vndjekornom@gmail.com>
2025-08-15 23:43:30 +0300

    Fixed a bug where trying to edit an Organization fit category item would throw a SQL error

--- backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/services/OrgFitCategoryItemService.java
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/services/OrgFitCategoryItemService.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface OrgFitCategoryItemService extends GenericService<OrgFitCategoryItem> {
 
+    public OrgFitCategoryItem merge(OrgFitCategoryItem entity);
 }
