7329c8050863a9590fe9771df5b77f9951a353d4
ndjek1 <vndjekornom@gmail.com>
2025-08-15 10:48:39 +0300

    Added the search unique by property method to GlobalWeightService and ReviewCycleService to allow retrieve only Global weight for only active review cycle

--- backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/services/impl/ReviewCycleService.java
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/services/impl/ReviewCycleService.java
@@ -4,5 +4,6 @@ import org.pahappa.systems.kpiTracker.core.services.GenericService;
 import org.pahappa.systems.kpiTracker.models.systemSetup.ReviewCycle;
 
 public interface ReviewCycleService extends GenericService<ReviewCycle> {
+    ReviewCycle searchUniqueByPropertyEqual(String property, Object value);
     Object getObjectById(String var1);
 }
