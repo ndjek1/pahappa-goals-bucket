c98f518094b7d3b43ac31d99da51df981689a111
ndjek1 <vndjekornom@gmail.com>
2025-08-18 10:53:40 +0300

    Added backend for managing organization goals, including , model, service and service implementation and a converter

---
+++ backend/kpi-tracker-models/src/main/java/org/pahappa/systems/kpiTracker/models/goals/OrganizationGoal.java
@@ -0,0 +1,69 @@
+package org.pahappa.systems.kpiTracker.models.goals;
+
+import org.pahappa.systems.kpiTracker.models.systemSetup.ReviewCycle;
+import org.sers.webutils.model.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "organization_goals")
+public class OrganizationGoal extends BaseEntity {
+    private String name;
+    private String description;
+    private double contributionWeight;
+    private ReviewCycle reviewCycle;
+
+    @Column(name = "name",nullable = false,unique = true)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "description",nullable = false)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "contribution_weight", nullable = false)
+    public double getContributionWeight() {
+        return contributionWeight;
+    }
+
+    public void setContributionWeight(double contributionWeight) {
+        this.contributionWeight = contributionWeight;
+    }
+    @ManyToOne
+    @JoinColumn(
+            name = "review_cycle_Id",
+            nullable = true
+    )
+    public ReviewCycle getReviewCycle() {
+        return reviewCycle;
+    }
+
+    public void setReviewCycle(ReviewCycle reviewCycle) {
+        this.reviewCycle = reviewCycle;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrganizationGoal)) return false;
+        OrganizationGoal that = (OrganizationGoal) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+
+}
