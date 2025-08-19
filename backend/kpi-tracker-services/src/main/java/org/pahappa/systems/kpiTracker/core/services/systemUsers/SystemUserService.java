49d693e96a14259d46a85482ad20d06dda12f31b
Peter Bukama Mulinzi <pbmulinzi@gmail.com>
2025-08-19 11:19:28 +0300

    The department members view page (page layout)

--- backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/services/systemUsers/SystemUserService.java
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/services/systemUsers/SystemUserService.java
@@ -1,7 +1,11 @@
 package org.pahappa.systems.kpiTracker.core.services.systemUsers;
 
 import org.pahappa.systems.kpiTracker.core.services.GenericService;
+import org.pahappa.systems.kpiTracker.models.organization_structure.Department;
 import org.pahappa.systems.kpiTracker.models.systemUsers.SystemUser;
 
+import java.util.List;
+
 public interface SystemUserService extends GenericService<SystemUser> {
+    public List<SystemUser> getMembersByDepartment(Department department);
 }
